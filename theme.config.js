8a16b9633ef23333ddc04816041368d74fa5f131
Mehul Mohan <mehulmpt@gmail.com>
2022-08-21 19:52:25 +0530

    codedamn docs

--- theme.config.js
+++ theme.config.js
@@ -6,7 +6,7 @@ export default {
     <>
       <span className="mr-2 font-extrabold hidden md:inline">codedamn</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        documentation
+        docs
       </span>
     </>
   ),
