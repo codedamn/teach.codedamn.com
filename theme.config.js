e134d7cb69dbcc1dcd9e2922a8f4b537cfad2d62
Mehul Mohan <mehulmpt@gmail.com>
2022-08-21 19:31:36 +0530

    Metadata fix

--- theme.config.js
+++ theme.config.js
@@ -1,12 +1,12 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/codedamn/use.codedamn.com',
+  docsRepositoryBase: 'https://github.com/codedamn/use.codedamn.com/blob/master',
+  titleSuffix: ' – codedamn',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">codedamn</span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        documentation
       </span>
     </>
   ),
@@ -16,14 +16,14 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="Documentation for using codedamn platform" />
+      <meta name="og:description" content="Documentation for using codedamn platform" />
       <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
+      {/* <meta name="twitter:image" content="https://nextra.vercel.app/og.png" /> */}
+      <meta name="twitter:site:domain" content="use.codedamn.com" />
+      <meta name="twitter:url" content="https://use.codedamn.com" />
+      <meta name="og:title" content="Documentation - codedamn" />
+      {/* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */}
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
         rel="apple-touch-icon"
