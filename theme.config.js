e17629181c037e7109313cff17455c8211b65860
Mehul Mohan <mehulmpt@gmail.com>
2022-08-31 22:43:08 +0530

    Metadata update

--- theme.config.js
+++ theme.config.js
@@ -1,67 +1,73 @@
 export default {
-  github: 'https://github.com/codedamn/use.codedamn.com',
-  docsRepositoryBase: 'https://github.com/codedamn/use.codedamn.com/blob/master',
-  titleSuffix: ' – codedamn',
-  logo: (
-    <>
-      <span className="mr-2 font-extrabold hidden md:inline">codedamn</span>
-      <span className="text-gray-600 font-normal hidden md:inline">
-        docs
-      </span>
-    </>
-  ),
-  head: (
-    <>
-      <meta name="msapplication-TileColor" content="#ffffff" />
-      <meta name="theme-color" content="#ffffff" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Documentation for using codedamn platform" />
-      <meta name="og:description" content="Documentation for using codedamn platform" />
-      <meta name="twitter:card" content="summary_large_image" />
-      {/* <meta name="twitter:image" content="https://nextra.vercel.app/og.png" /> */}
-      <meta name="twitter:site:domain" content="use.codedamn.com" />
-      <meta name="twitter:url" content="https://use.codedamn.com" />
-      <meta name="og:title" content="Documentation - codedamn" />
-      {/* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */}
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href="/apple-icon-180x180.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="192x192"
-        href="/android-icon-192x192.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href="/favicon-32x32.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="96x96"
-        href="/favicon-96x96.png"
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href="/favicon-16x16.png"
-      />
-      <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-    </>
-  ),
-  search: true,
-  prevLinks: true,
-  nextLinks: true,
-  footer: true,
-  footerEditLink: 'Edit this page on GitHub',
-  footerText: <>Copyright {new Date().getFullYear()} © codedamn. All rights reserved.</>,
-  unstable_faviconGlyph: '👋',
+	github: 'https://github.com/codedamn/use.codedamn.com',
+	docsRepositoryBase:
+		'https://github.com/codedamn/use.codedamn.com/blob/master',
+	titleSuffix: ' – codedamn',
+	logo: (
+		<>
+			<span className="mr-2 font-extrabold hidden md:inline">
+				codedamn
+			</span>
+			<span className="text-gray-600 font-normal hidden md:inline">
+				docs
+			</span>
+		</>
+	),
+	head: (
+		<>
+			<meta name="msapplication-TileColor" content="#ffffff" />
+			<meta name="theme-color" content="#ffffff" />
+			<meta
+				name="viewport"
+				content="width=device-width, initial-scale=1.0"
+			/>
+			<meta httpEquiv="Content-Language" content="en" />
+			<meta
+				name="description"
+				content="Documentation for creators using codedamn platform"
+			/>
+			<meta
+				name="og:description"
+				content="Documentation for creators using codedamn platform"
+			/>
+			<meta name="twitter:card" content="summary_large_image" />
+			{/* <meta name="twitter:image" content="https://nextra.vercel.app/og.png" /> */}
+			<meta name="twitter:site:domain" content="use.codedamn.com" />
+			<meta name="twitter:url" content="https://use.codedamn.com" />
+			<meta name="og:title" content="Documentation - codedamn" />
+			{/* <meta name="og:image" content="https://nextra.vercel.app/og.png" /> */}
+			<meta name="apple-mobile-web-app-title" content="Codedamn" />
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="32x32"
+				href={getFavicon(32)}
+			/>
+			<link
+				rel="icon"
+				type="image/png"
+				sizes="16x16"
+				href={getFavicon(16)}
+			/>
+			<link rel="manifest" href="/manifest.json" />
+			<link rel="shortcut icon" href={getFavicon(96)} />
+
+			<meta
+				name="msapplication-TileImage"
+				content="/ms-icon-144x144.png"
+			/>
+		</>
+	),
+	search: true,
+	prevLinks: true,
+	nextLinks: true,
+	footer: true,
+	footerEditLink: 'Edit this page on GitHub',
+	footerText: (
+		<>
+			Copyright {new Date().getFullYear()} © codedamn. All rights
+			reserved.
+		</>
+	),
+	unstable_faviconGlyph: '💻'
 }
